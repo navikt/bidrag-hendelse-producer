9853a3b8a1d9b9855a39f9cfb48a346e2b123cfd
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-12 14:24:22 +0200

    init kotlin project for kafka bidrag producer

---
+++ src/main/kotlin/no/nav/bidrag/hendelse/producer/BidragHendelseProducerApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.bidrag.hendelse.producer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BidragHendelseProducerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BidragHendelseProducerApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/bidrag/hendelse/producer/BidragHendelseProducerApplicationTests.kt
@@ -0,0 +1,14 @@
+package no.nav.bidrag.hendelse.producer;
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BidragHendelseProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+
+	}
+
+}
