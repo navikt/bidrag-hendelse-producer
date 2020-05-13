a3a03e22e7410d76ac8bd70d2c95477a6514bb3f
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-13 13:14:36 +0200

    add test class for producer

---
+++ src/test/kotlin/no/nav/bidrag/hendelse/producer/ProducerTest.kt
@@ -0,0 +1,13 @@
+package no.nav.bidrag.hendelse.producer
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+class ProducerTest {
+
+    @Test
+    fun test(){
+        Assertions.assertTrue(true);
+    }
+}
