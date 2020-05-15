8c909185119bbd6e2fb1d76fa47109c641b50051
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-15 13:10:23 +0200

    add github workflow to deploy. Add nais setup for kafka properties. Add correct topic name.

--- src/main/kotlin/no/nav/bidrag/hendelse/producer/Producer.kt
+++ src/main/kotlin/no/nav/bidrag/hendelse/producer/Producer.kt
@@ -3,6 +3,7 @@ package no.nav.bidrag.hendelse.producer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Service
 class Producer (@Autowired val kafkaTemplate: KafkaTemplate<String,String>){
 
     private val logger: Logger = LoggerFactory.getLogger(Producer::class.java)
-    private val TOPIC = "test" // Plasser i yaml filen eller fra NAIS.
+    @Value("\${app.topic}")
+    private lateinit var TOPIC:String
 
     fun sendMessage(message: String?) {
         logger.info(String.format("$$ -> Produserer melding --> %s", message))
