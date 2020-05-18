903a28122ec56a2922714538f59968f99facb31b
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-18 11:41:49 +0200

    remove unwanted and unsued username and password for ABBAC

--- init-scripts/export-service-user.sh
+++ init-scripts/export-service-user.sh
@@ -1,35 +1,5 @@
 #!/usr/bin/env bash
 
-if test -f /var/run/secrets/nais.io/srvbisys-dev/username;
-then
-    export  ABAC_USERNAME=$(cat /var/run/secrets/nais.io/srvbisys-dev/username)
-else
-  if test -f /var/run/secrets/nais.io/srvbisys-prod/username;
-  then
-      export  ABAC_USERNAME=$(cat /var/run/secrets/nais.io/srvbisys-prod/username)
-  fi
-fi
-
-if test -f /var/run/secrets/nais.io/srvbisys-dev/password;
-then
-    export  ABAC_PASSWORD=$(cat /var/run/secrets/nais.io/srvbisys-dev/password)
-else
-  if test -f /var/run/secrets/nais.io/srvbisys-prod/password;
-  then
-      export  ABAC_PASSWORD=$(cat /var/run/secrets/nais.io/srvbisys-prod/password)
-  fi
-fi
-
-if test -f /var/run/secrets/nais.io/srvbisys-prod/username;
-then
-    export  PIP_USERNAME=$(cat /var/run/secrets/nais.io/srvbisys-prod/username)
-fi
-
-if test -f /var/run/secrets/nais.io/srvbisys-prod/password;
-then
-    export  PIP_PASSWORD=$(cat /var/run/secrets/nais.io/srvbisys-prod/password)
-fi
-
 if test -f /var/run/secrets/nais.io/srvbidrag-dokument/username;
 then
     export  SRVBIDRAG_DOKUMENT_USERNAME=$(cat /var/run/secrets/nais.io/srvbidrag-dokument/username)
