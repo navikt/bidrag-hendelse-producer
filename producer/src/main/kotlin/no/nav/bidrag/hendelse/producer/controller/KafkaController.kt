769e5c6c107b32e6d07e63628982e60a2aad5b0f
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-19 13:25:13 +0200

    split project into modules for use of avro and kafka producer

--- producer/src/main/kotlin/no/nav/bidrag/hendelse/producer/controller/KafkaController.kt
+++ producer/src/main/kotlin/no/nav/bidrag/hendelse/producer/controller/KafkaController.kt
@@ -1,5 +1,4 @@
 package no.nav.bidrag.hendelse.producer.controller
-import no.nav.bidrag.hendelse.producer.dto.RegistrerJournalpostDto
 import no.nav.bidrag.hendelse.producer.kafka.Producer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
