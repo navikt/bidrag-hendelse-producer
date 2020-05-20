5a8c38b0ce1a88248c710e989c9f30d3fb36a664
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-20 13:58:32 +0200

    remove unused log imports

--- producer/src/main/kotlin/no/nav/bidrag/hendelse/producer/kafka/RegistrertJournalpostMeldingProducer.kt
+++ producer/src/main/kotlin/no/nav/bidrag/hendelse/producer/kafka/RegistrertJournalpostMeldingProducer.kt
@@ -1,8 +1,6 @@
 package no.nav.bidrag.hendelse.producer.kafka
 
 import no.nav.bidrag.hendelse.producer.dto.RegistrerJournalpostDto
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
