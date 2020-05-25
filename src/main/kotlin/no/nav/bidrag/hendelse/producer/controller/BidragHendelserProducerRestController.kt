94abc903422b99b9252caf7394f70aed394b0100
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2020-05-25 14:42:39 +0200

    add logging to rest controller and producer

--- src/main/kotlin/no/nav/bidrag/hendelse/producer/controller/BidragHendelserProducerRestController.kt
+++ src/main/kotlin/no/nav/bidrag/hendelse/producer/controller/BidragHendelserProducerRestController.kt
@@ -1,5 +1,7 @@
 package no.nav.bidrag.hendelse.producer.controller
 import no.nav.bidrag.hendelse.producer.kafka.RegistrertJournalpostMeldingProducer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/hendelse")
 class BidragHendelserProducerRestController(@Autowired val registrertJournalpostMeldingProducer: RegistrertJournalpostMeldingProducer){
 
+    private val LOGGER: Logger = LoggerFactory.getLogger(BidragHendelserProducerRestController::class.java)
+
     @PostMapping("publish/journalpost/registrert")
     fun publishRegistrertJournalpostMeldingPaaTopic(@RequestBody registrerJournalpostForm:RegisterJournalpostForm){
+        LOGGER.info(String.format("Fikk en POST request om a publisere melding paa topic med journalpostid: %s og saksummer %s. ",
+                registrerJournalpostForm.journalpostid, registrerJournalpostForm.saksnummer));
         registrertJournalpostMeldingProducer.sendMelding(registrerJournalpostForm.journalpostid,registrerJournalpostForm.saksnummer);
     }
 
--- src/main/kotlin/no/nav/bidrag/hendelse/producer/kafka/RegistrertJournalpostMeldingProducer.kt
+++ src/main/kotlin/no/nav/bidrag/hendelse/producer/kafka/RegistrertJournalpostMeldingProducer.kt
@@ -1,6 +1,8 @@
 package no.nav.bidrag.hendelse.producer.kafka
 
 import no.nav.bidrag.hendelse.producer.dto.RegistrerJournalpostDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
@@ -9,10 +11,13 @@ import org.springframework.stereotype.Service
 @Service
 class RegistrertJournalpostMeldingProducer (@Autowired val kafkaTemplate: KafkaTemplate<String,RegistrerJournalpostDto>){
 
+    private val LOGGER: Logger = LoggerFactory.getLogger(RegistrertJournalpostMeldingProducer::class.java)
+
     @Value("\${kafka.topic}")
     private lateinit var TOPIC:String
 
     fun sendMelding(journalpostid:String, saksnummer:String) {
-        this.kafkaTemplate.send(TOPIC,RegistrerJournalpostDto(journalpostid, saksnummer));
+        LOGGER.info(String.format("Publiserer melding paa topic med journalpostid %s og saksnummer %s.", journalpostid, saksnummer));
+        this.kafkaTemplate.send(TOPIC, RegistrerJournalpostDto(journalpostid, saksnummer));
     }
 }
